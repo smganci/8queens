e047d9507416fc96130dfcde93168a64b3b62d8a
Sarah Ganci <sarahganci@gmail.com>
2019-01-26 18:35:15 -0500

    changes

--- app/src/main/java/com/example/eightqueens/MainActivity.java
+++ app/src/main/java/com/example/eightqueens/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.eightqueens;
 
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.graphics.Point;
+import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -13,6 +17,7 @@ import android.widget.Button;
 import android.widget.GridLayout;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -20,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
 
     protected boolean[][] bgrid;
     protected ImageButton[][] buttons;
+    protected boolean invalid_move;
+    protected boolean stop;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,6 +34,9 @@ public class MainActivity extends AppCompatActivity {
 
         bgrid= new boolean[8][8];
         buttons= new ImageButton[8][8];
+        invalid_move=false;
+        stop=false;
+        stop=false;
         ViewGroup linearLayout = (ViewGroup) findViewById(R.id.layout);
         GridLayout grid= (GridLayout) findViewById(R.id.grid);
 
@@ -38,56 +48,76 @@ public class MainActivity extends AppCompatActivity {
             for(int j=0;j<8;j++){
 
                 //create button
-                ImageButton button= new ImageButton(getApplicationContext());
+                final ImageButton button= new ImageButton(getApplicationContext());
                 buttons[i][j]=button;
+                //set initial background based on row and column
+                draw_tile(i,j);
 
-                if(j%2==0 && i%2==0 ||j%2==1&&i%2==1){
-                    button.setBackgroundResource(R.drawable.lp_tile);
-                    final int i_point=i;
-                    final int j_point=j;
+                final int i_point=i;
+                final int j_point=j;
 
-                    button.setOnClickListener(new View.OnClickListener() {
-                        public void onClick(View v){
+                button.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v){
+                        check_and_update(i_point,j_point);
+                    }
+                });
 
-                                if(bgrid[i_point][j_point]){
-                                    v.setBackgroundResource(R.drawable.lp_tile);
-                                    bgrid[i_point][j_point]=false;
-                                }else{
-                                    if(check_place(i_point,j_point)){
-                                        v.setBackgroundResource(R.drawable.lp_queen);
-                                        bgrid[i_point][j_point]=true;
-                                    }else{
-                                        Toast.makeText(MainActivity.this, "That move is invalid", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                        }
-                    });
-
-
-                }else {
-                    button.setBackgroundResource(R.drawable.dp_tile);
-                    final int i_point=i;
-                    final int j_point=j;
-
-                    button.setOnClickListener(new View.OnClickListener() {
-                        public void onClick(View v){
-
-                                if(bgrid[i_point][j_point]){
-                                    v.setBackgroundResource(R.drawable.dp_tile);
-                                    bgrid[i_point][j_point]=false;
-                                }else{
-                                    //if place is valid, play
-                                    if(check_place(i_point,j_point)){
-                                        v.setBackgroundResource(R.drawable.dp_queen);
-                                        bgrid[i_point][j_point]=true;
-                                    }else{
-                                        Toast.makeText(MainActivity.this, "That move is invalid", Toast.LENGTH_SHORT).show();
-                                    }
-                                }
-                        }
-                    });
-
-                }
+//
+//
+//                if(j%2==0 && i%2==0 ||j%2==1&&i%2==1){
+//                    button.setBackgroundResource(R.drawable.lp_tile);
+//                    final int i_point=i;
+//                    final int j_point=j;
+//
+//                    button.setOnClickListener(new View.OnClickListener() {
+//                        public void onClick(View v){
+//
+//                            Toast.makeText(MainActivity.this, "TEST", Toast.LENGTH_SHORT);
+//                                if(bgrid[i_point][j_point]){
+//                                    v.setBackgroundResource(R.drawable.lp_tile);
+//                                    bgrid[i_point][j_point]=false;
+//                                }else{
+//                                    if(check_place(i_point,j_point)){
+//                                        v.setBackgroundResource(R.drawable.lp_queen);
+//                                        bgrid[i_point][j_point]=true;
+//                                    }else{
+//                                        //if place is valid, play
+//                                        if(check_place(i_point,j_point)){
+//                                            v.setBackgroundResource(R.drawable.dp_queen);
+//                                            bgrid[i_point][j_point]=true;
+//                                        }else{
+//                                            Toast.makeText(MainActivity.this, "INVALID", Toast.LENGTH_SHORT);
+//                                        }
+//                                    }
+//                                }
+//                        }
+//                    });
+//
+//
+//                }else {
+//                    button.setBackgroundResource(R.drawable.dp_tile);
+//                    final int i_point=i;
+//                    final int j_point=j;
+//
+//                    button.setOnClickListener(new View.OnClickListener() {
+//                        public void onClick(View v){
+//
+//                                if(bgrid[i_point][j_point]){
+//                                    v.setBackgroundResource(R.drawable.dp_tile);
+//                                    bgrid[i_point][j_point]=false;
+//                                }else{
+//                                    //if place is valid, play
+//                                    if(check_place(i_point,j_point)){
+//                                        v.setBackgroundResource(R.drawable.dp_queen);
+//                                        bgrid[i_point][j_point]=true;
+//                                    }else{
+//                                        Toast.makeText(MainActivity.this, "INVALID", Toast.LENGTH_SHORT);
+//                                    }
+//                                }
+//                        }
+//                    });
+//
+//                }
                 grid.addView((button));
 
 
@@ -111,24 +141,36 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+
+    protected void draw_and_set_tile(int i, int j, boolean queen){
+        bgrid[i][j]=queen;
+        draw_tile(i,j);
+    }
+
+
+    protected void draw_tile(int i, int j){
+        if(j%2==0 && i%2==0 ||j%2==1&&i%2==1) {
+            if(bgrid[i][j]){
+                buttons[i][j].setBackgroundResource(R.drawable.lp_queen);
+            }else {
+                buttons[i][j].setBackgroundResource(R.drawable.lp_tile);
+            }
+        }else{
+            if(bgrid[i][j]){
+                buttons[i][j].setBackgroundResource(R.drawable.dp_queen);
+            }else {
+                buttons[i][j].setBackgroundResource(R.drawable.dp_tile);
+            }
+        }
+
+    }
+
     protected void draw_board(boolean[][]answer){
         this.bgrid=answer;
 
         for(int i=0;i<8;i++){
             for(int j=0;j<8;j++){
-                if(j%2==0 && i%2==0 ||j%2==1&&i%2==1) {
-                    if(bgrid[i][j]){
-                        buttons[i][j].setBackgroundResource(R.drawable.lp_queen);
-                    }else {
-                        buttons[i][j].setBackgroundResource(R.drawable.lp_tile);
-                    }
-                }else{
-                    if(bgrid[i][j]){
-                        buttons[i][j].setBackgroundResource(R.drawable.dp_queen);
-                    }else {
-                        buttons[i][j].setBackgroundResource(R.drawable.dp_tile);
-                    }
-                }
+                draw_tile(i,j);
             }
         }
 
@@ -165,22 +207,49 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void check_and_update(int x, int y){
+        if(this.stop){
+            return;
+        }
+        if(bgrid[x][y]){
+            TextView message= (TextView) findViewById(R.id.message_box);
+            message.setText("You have unbselected a queen");
+            draw_and_set_tile(x,y,false);
+
+        }else if(check_place(x, y)){
+            draw_and_set_tile(x,y,true);
+            TextView message= (TextView) findViewById(R.id.message_box);
+            if(this.count_queens()<8){
+                int remaining=8-this.count_queens();
+                message.setText("Good Move! Only "+ remaining+ " more Queens to place!");
+            }else{
+                message.setText("You Win!!! Press \"Restart\" to play again!");
+                this.stop=true;
+            }
+
+        }else{
+            //get warning box and update for invalid move
+            TextView message= (TextView) findViewById(R.id.message_box);
+            message.setText("Invalid Move! Try again.");
+        }
+    }
 
 
     protected void restart(View v){
+        this.stop=false;
         this.draw_board(new boolean[8][8]);
-//        for(int i=0;i<8;i++){
-//            for(int j=0;j<8;j++){
-//
-//                bgrid[i][j]=false;
-//                if(j%2==0 && i%2==0 ||j%2==1&&i%2==1) {
-//                    buttons[i][j].setBackgroundResource(R.drawable.lp_tile);
-//                }else{
-//                    buttons[i][j].setBackgroundResource(R.drawable.dp_tile);
-//                }
-//
-//            }
-//        }
+    }
+
+    protected int count_queens(){
+        int sum=0;
+        for(int i=0;i<8;i++){
+            for(int j=0;j<8;j++){
+                if(bgrid[i][j]){
+                    sum++;
+                }
+            }
+        }
+        return sum;
     }
 
 
